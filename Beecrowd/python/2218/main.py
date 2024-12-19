8842d38ffff114df5962923c2572a97d1a1abb02
Jean Lima <132618223+JeanLima2112@users.noreply.github.com>
2024-12-19 18:45:35 -0300

    add beecrowd exercises

---
+++ Beecrowd/python/2218/main.py
@@ -0,0 +1,8 @@
+N = int(input())
+
+for _ in range(N):
+    X = int(input())
+    value = ((X * (X + 1)) // 2) +1
+    print(value)
+
+# este exercicio foi resolvido encontrando a formula fecha para a recursiva que foi evidenciada no exercicio
---
+++ Beecrowd/python/2879/main.py
@@ -0,0 +1,7 @@
+N = int(input())
+ganhou = 0
+for c in range(0,N):
+    X = int(input())
+    if X != 1:
+        ganhou += 1
+print(ganhou)
