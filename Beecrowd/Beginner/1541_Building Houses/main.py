a0bce95c49a839d1e403f6df0da3457ca927e888
Jean Lima <jeanlima5939@gmail.com>
2025-02-06 15:08:16 -0300

    add Becrowd exercises

--- Beecrowd/Beginner/1541_Building Houses/main.py
+++ Beecrowd/Beginner/1541_Building Houses/main.py
@@ -1,1 +1,9 @@
 #Solution in Python
+from math import floor
+while True:
+    X = list(map(int, input().split()))
+    if X[0] == 0:
+        break
+    print(floor(((((X[0]* X[1])* 100))/X[2])**0.5))
+
+        
